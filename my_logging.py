d285791823db33af462ec5c27f3c225f4dc12869
0xbadc0ffe <m.viscione98@gmail.com>
2020-07-23 12:01:37 -0400

    log update

--- my_logging.py
+++ my_logging.py
@@ -10,7 +10,7 @@ class Log:
     # passato, ha inizio da time.time().
     # Apre il file passatogli tramite parametro
     # e vi appende una linea iniziale.
-    def __init__(self, logfile, time_start=time.time()):
+    def __init__(self, logfile="logfile.log", time_start=time.time()):
         if (time_start == None):
             time_start = time.time()
         self.time_start = time_start
@@ -23,21 +23,25 @@ class Log:
     # passatagli via parametro preceduta da
     # una time string basata sul tempo reale
     # (datetime.now)
-    def uplog(self, s):
+    # new_line descrive il numero di \n da concatenare alla stringa in input
+    # (default 1)
+    def uplog(self, s, new_line=1):
         s = str(s)
         now = datetime.now()
         ts = now.strftime("%d/%m/%Y %H:%M:%S")  # time string
-        print("[" + ts + "]: " + s + "\n")
-        self.logfile.write("["+ts+"]: "+ s +"\n")
+        print("[" + ts + "]: " + s + "\n"*new_line) # la print aggiunge un \n di suo, ma a schermo va bene
+        self.logfile.write("["+ts+"]: "+ s +"\n"*new_line)
 
     # Funzione di Relative Time Update Log:
     # Stampa a video e nel file la stringa
     # passatagli via parametro preceduta da
     # una time string basata sul tempo
     # relativo dall'avvio del logging.
-    def rt_uplog(self, s): #relative time uplog
+    # new_line descrive il numero di \n da concatenare alla stringa in input
+    # (default 1)
+    def rt_uplog(self, s, new_line=1): #relative time uplog
         s = str(s)
-        rts = "[%.3f]: " + s + "\n"             # relative time string
+        rts = "[%.3f]: " + s + "\n"*new_line             # relative time string
         print(rts % (time.time() - self.time_start)) 
         self.logfile.write(rts % (time.time() - self.time_start))
 
@@ -45,10 +49,12 @@ class Log:
     # Stampa a video e nel file la stringa
     # passatagli via parametro, senza
     # indicazioni sul tempo.
-    def nt_uplog(self,s): #no time uplog
+    # new_line descrive il numero di \n da concatenare alla stringa in input
+    # (default 1)
+    def nt_uplog(self,s,new_line=1): #no time uplog
         s = str(s)
-        print(s)
-        self.logfile.write(s + "\n")
+        print(s+'\n'*(new_line-1))
+        self.logfile.write(s + "\n"*new_line)
 
     # Funzione di chiusura logging:
     # Appende nel file una linea di
