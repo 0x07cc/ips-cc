fc7b65410ced902e9689bbb0f3ee7f9dd0dee835
0x07cc <0x07cc@users.noreply.github.com>
2020-07-25 22:41:50 +0200

    Added pcap export for dropped packets

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ import my_logging as mylog
 import my_analysis
 import utils
 #import packet_handling
+import pcap
 
 # Parametri
 numero_queue = 33
 logfile = "logfile.log"
+pcapfile= "dropped_packets.pcap"
 regex_list = ['CC{\w+}','CCRU{\w+}','doveva annà così fratellì','https://www.youtube.com/watch?v=dQw4w9WgXcQ']  # Lista di regex e stringhe bannate
 service_type = 'Netcat' # Tipo di servizio, per ora e' rappresentato dal nome
 
@@ -22,65 +24,65 @@ service_type = 'Netcat' # Tipo di servizio, per ora e' rappresentato dal nome
 # fornita, decide se lasciar passare il
 # pacchetto o rifiutarlo.
 def gestisci_pacchetto(pkt):
-	payload = pkt.get_payload()
-	payload_hex = payload.hex()
+    payload = pkt.get_payload()
+    payload_hex = payload.hex()
 
-	if debug:
-		log.nt_uplog('-------------')
-		try:
-			log.uplog('Data received: '+ payload[52:-1].decode('utf-8'))
-		except UnicodeDecodeError:
-			log.uplog("Can't decode received data")
+    if debug:
+        log.nt_uplog('-------------')
+        try:
+            log.uplog('Data received: '+ payload[52:-1].decode('utf-8'))
+        except UnicodeDecodeError:
+            log.uplog("Can't decode received data")
 
-	# Verifica della versione di IP del pacchetto:
-	# Se non e' 4, non effettuo controlli
-	# e lo accetto.
-	versioneIP = payload_hex[0]
-	if versioneIP != '4':
-		log.uplog("Received a non-IPv4 packet, accepting it")
-		pkt.accept()
-		return
+    # Verifica della versione di IP del pacchetto:
+    # Se non e' 4, non effettuo controlli
+    # e lo accetto.
+    versioneIP = payload_hex[0]
+    if versioneIP != '4':
+        log.uplog("Received a non-IPv4 packet, accepting it")
+        pkt.accept()
+        return
 
-	inizioTCP = utils.calcola_lunghezza_ipv4(payload_hex[1])
-	# TODO: verificare se SYN e' settato, in tal caso -> accept()
+    inizioTCP = utils.calcola_lunghezza_ipv4(payload_hex[1])
+    # TODO: verificare se SYN e' settato, in tal caso -> accept()
 
-	if debug:
-		ipSource = payload_hex[24:32]
-		ipDest = payload_hex[32:40]
-		log.uplog("Source IPv4: " + utils.calcolaIPv4(ipSource)+ "  Destination IPv4: " + utils.calcolaIPv4(ipDest))
-		portaSource = payload[inizioTCP:inizioTCP+2].hex()
-		portaSourceint = int(portaSource, 16)
-		portaDest = payload[inizioTCP+2:inizioTCP+4].hex()
-		portaDestint = int(portaDest, 16)
-		log.uplog("Source port: " + str(portaSourceint) + "  Destination Port: " + str(portaDestint))
-		log.uplog(pkt)
-		#print(payload_hex)
-		log.uplog(payload)
-		log.nt_uplog('-------------')
+    if debug:
+        ipSource = payload_hex[24:32]
+        ipDest = payload_hex[32:40]
+        log.uplog("Source IPv4: " + utils.calcolaIPv4(ipSource)+ "  Destination IPv4: " + utils.calcolaIPv4(ipDest))
+        portaSource = payload[inizioTCP:inizioTCP+2].hex()
+        portaSourceint = int(portaSource, 16)
+        portaDest = payload[inizioTCP+2:inizioTCP+4].hex()
+        portaDestint = int(portaDest, 16)
+        log.uplog("Source port: " + str(portaSourceint) + "  Destination Port: " + str(portaDestint))
+        log.uplog(pkt)
+        #print(payload_hex)
+        log.uplog(payload)
+        log.nt_uplog('-------------')
 
 
-	# Ricerca dell'espressione regolare
-	match = shield.is_droppable(payload)
-	if match:
-		pkt.drop()
-		log.uplog("Packet dropped")
-	else:
-		pkt.accept()
+    # Ricerca dell'espressione regolare
+    match = shield.is_droppable(payload)
+    if match:
+        pkt.drop()
+        pcap_exporter.make_packet_record(payload_hex)
+        log.uplog("Packet dropped and added to pcap")
+    else:
+        pkt.accept()
 
 # Verifica che l'utente sia root
 if not utils.is_root():
-	print("You need root privileges to run this application!")
-	exit()
+    print("You need root privileges to run this application!")
+    exit()
 
 # Verifica se il programma e' stato
 # avviato con il flag di debug
 debug = utils.is_debug()
 
-# Creazione oggetto di classe Log
+# Creazione oggetti di classe Log, Shield e PCAP
 log = mylog.Log(logfile)
-
-# Creazione oggetto di classe Shield
 shield = my_analysis.Shield(regex_list, service_type, log)
+pcap_exporter = pcap.PCAP(log, pcapfile)
 
 
 log.uplog("Starting ips-cc")
@@ -94,9 +96,9 @@ nfqueue = NetfilterQueue()
 nfqueue.bind(numero_queue, gestisci_pacchetto)
 
 try:
-	nfqueue.run()
+    nfqueue.run()
 except KeyboardInterrupt:
-	log.uplog("Received Interrupt, shutting down")
+    log.uplog("Received Interrupt, shutting down")
 
 nfqueue.unbind()
 log.uplog("Stopped ips-cc")
---
+++ pcap.py
@@ -0,0 +1,76 @@
+# PCAP export module
+import time
+import binascii
+
+class PCAP:
+
+    # Funzione costruttore dell'oggetto.
+    # Al momento distrugge il vecchio file PCAP,
+    # crea un header PCAP e lo appende al file.
+    # Prende in input un oggetto di classe Log e una
+    # stringa contenente il nome del file di output.
+    def __init__(self, log, filename="dropped_packets.pcap"):
+        self.log = log
+        log.uplog("Starting PCAP exporting module")
+
+        try:
+            self.outputFile = open(filename,"wb")
+        except:
+            log.uplog("Error while opening " + filename)
+            exit()
+
+        self.header = self.make_header()
+        self.outputFile.write(header)
+
+    # Funzione che ritorna bytes dell'header PCAP.
+    # length e' un parametro al momento non
+    # usato che dovrebbe influenzare SnapLen.
+    # Documentazione di riferimento:
+    # http://xml2rfc.tools.ietf.org/cgi-bin/xml2rfc.cgi?url=https://raw.githubusercontent.com/pcapng/pcapng/master/draft-gharris-opsawg-pcap.xml&modeAsFormat=html/ascii&type=ascii#packet_record
+    def make_header(self, length=4):
+        header  = 'D4C3B2A1' # Magic Number in little endian
+        header += '02000400' # Version 2.4
+        header += '00000000' # Reserved 1
+        header += '00000000' # Reserved 2
+
+        # SnapLen: maximum number of octets
+        # captured from each packet
+        header += '00000400' #TODO: capire se va aumentato
+
+        # LinkType: Raw IPv4
+        # Documentazione di riferimento:
+        # http://www.tcpdump.org/linktypes.html
+        header += 'e4000000' # e4 (Hex) = 228 (Dec)
+
+        header_byte = binascii.unhexlify(header)
+        return header_byte
+
+    # Funzione che aggiunge un record al file PCAP.
+    # Richiede in ingresso una stringa contenente
+    # un pacchetto IP (In genere inizia con '450000')
+    def make_packet_record(IP_packet):
+        # Seconds and microseconds
+        time_hex = hex(int(time.time()))[2:10] # Seconds from epoch
+        time_1= time_hex[0:2]
+        time_2= time_hex[2:4]
+        time_3= time_hex[4:6]
+        time_4= time_hex[6:8]
+        time_dec = '00000000' # We don't care about ms
+        packet_record = time_4 + time_3 + time_2 + time_1 + time_dec
+
+        # Packet Length
+        # IP_packet is a string, a byte is represented by two characters.
+        packet_length_int = len(IP_packet)//2
+        packet_length_hex = hex(packet_length_int)[2:]
+        # Fill the rest with zero
+        packet_length = packet_length_hex + '0'*(8-len(packet_length_hex))
+        packet_record += packet_length # Captured Packet Length
+        packet_record += packet_length # Original Packet Length
+
+        # IP Packet
+        packet_record += IP_packet
+
+        packet_record_bytes = binascii.unhexlify(packet_record)
+        self.outputFile.write(packet_record_bytes)
+
+#make_pcap('4500003c7b8d4000400633c7c0a80063041fc63dd6b00050eecf76d900000000a002faf0a4300000020405b40402080a9a7a46520000000001030307')
